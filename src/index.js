76767feb276b41e927574adf07e47723ffe65ea3
Rainie <wsc22@ic.ac.uk>
2024-06-04 01:53:02 +0800

    import

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import reportWebVitals from './reportWebVitals';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {HashRouter} from "react-router-dom";
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         </HashRouter>
     </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
